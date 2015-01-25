4cca7388cd7bec3f1b98bae05c03869ecb589809
mettledrum <mettledrum@gmail.com>
2015-01-25 11:31:39 -0700

    add dynamic gif array / bootstrap / can't get post to work

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 (function() {
-	var app = angular.module('pongApp', []);
+	var app = angular.module('pongApp', ['ngSanitize']);
 
 	app.controller('PicController', ['$http', function ($http) {
 		var picServer = this;
@@ -7,6 +7,11 @@
 		picServer.errors = [];
 
 		var allPicsEndpoint = 'http://localhost:8080/pong-pics/';
+		var gifizeEndpoint = 'http://localhost:8080/gifize';
+
+		picServer.gifPics = [];
+		picServer.gifPicTimes = [];
+		var gifPicIdx = 0;
 
 		// parse pic file names from server
 		$http.get(allPicsEndpoint).
@@ -28,5 +33,29 @@
 			error(function() {
 				picServer.errors.push("Darn... could not retrieve pictures from pong server.")
 			});
+
+		picServer.addFile = function(imageName, imageTime) {
+			// id is needed to make pictures' html strings unique
+			picHtml = '<img id="' + gifPicIdx.toString() + '" class="small-image" src="' + imageName + '"/>'
+			picServer.gifPics.push(picHtml);
+			picServer.gifPicTimes.push(imageTime);
+			gifPicIdx++;
+		};
+
+		picServer.clearFiles = function() {
+			picServer.gifPics = [];
+			picServer.gifPicTimes = [];
+			gifPicIdx = 0;
+		};
+
+		picServer.gifize = function() {
+			// $http.post(gifizeEndpoint, {msg: 'ahoy mundo!'}).
+			// 	success(function() {
+			// 		console.log('worked');
+			// 	}).
+			// 	error(function() {
+			// 		console.log('did not work');
+			// 	});
+		};
 	}]);
 })();
